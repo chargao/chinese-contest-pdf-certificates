38dcc1e0a565293af101e2c3200dbfae2b311d4b
Charles Gao <cg@charless-mbp.lan>
2019-09-23 00:34:34 -0500

    some refactoring

--- gen_pdf_cert.py
+++ gen_pdf_cert.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 from reportlab.lib.pagesizes import letter, landscape
 from reportlab.lib.units import inch
-from reportlab.lib.colors import Color
+from reportlab.lib.colors import Color, black
 from reportlab.pdfgen import canvas
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
@@ -15,17 +15,13 @@ title_cn = "第八届黄河杯中文有奖阅读竞赛"
 title_en = "THE 8TH YELLOW RIVER CUP CHINESE READING CONTEST"
 
 committee_cn="北美黄河杯中文竞赛委员会"
-committee_en="NORTH AMERICAN YELLOW RIVER CUP"
-committee_en2 ="CHINESE CONTEST COMMITTEE"
+committee_en=["NORTH AMERICAN YELLOW RIVER CUP","CHINESE CONTEST COMMITTEE"]
 date_of_completion="09/30/2019"
 
 level_1_cn="初级"
 level_2_cn="中级"
 
-first_place="一等奖 FIRST PLACE"
-second_place="二等奖 SECOND PLACE"
-third_place="三等奖 THIRD PLACE"
-completion="完成 COMPLETION"
+places=["一等奖 FIRST PLACE","二等奖 SECOND PLACE","三等奖 THIRD PLACE","完成 COMPLETION"]
 
 #calculations
 half_x = 5.5*inch
@@ -48,23 +44,10 @@ def generate_certificate(entry):
 		return
 
 	c = canvas.Canvas("results/" + name + ".pdf", pagesize=landscape(letter))
-
-	#background
-	img_path = 'images/background.jpg'
-	c.drawImage(image=img_path,x=0.25*inch,y=0.25*inch,width=10.5*inch,height=8.0*inch, mask='auto')
-	
-	#gold border
-	c.setStrokeColorRGB(0.999,0.799,0.066)
-	c.setFillColor(Color(100,100,100, alpha=0.25))
-	c.setLineWidth(3)
-	c.rect(0.0625*inch,0.0625*inch,10.875*inch,8.375*inch,fill=1)
-	c.setStrokeColorRGB(1.0,0.8,0.067)
-	c.setFillColor(Color(100,100,100, alpha=0.1))
-	c.setLineWidth(1)
-	c.rect(0.125*inch,0.125*inch,10.75*inch,8.25*inch,fill=1)
+	draw_static_elements(c)
 	
 	#title	
-	c.setStrokeColorRGB(0.0,0.0,0.0)
+	c.setStrokeColor(black)
 	c.setFillColor(Color(0,0,0, alpha=1.0))
 	c.setFont('Helvetica', 24)
 	c.drawCentredString(half_x,7.5*inch, "CERTIFICATE OF ACHIEVEMENT")
@@ -76,15 +59,10 @@ def generate_certificate(entry):
 	c.drawCentredString(half_x,6.5*inch, title_en)
 
 	#name
-	c.setStrokeColorRGB(0.0,0.0,0.0)
-	c.setFillColorRGB(0.0,0.0,0.0)
+	c.setStrokeColor(black)
+	c.setFillColor(black)
 	c.setFont('STHeiti', 30)
 	c.drawCentredString(half_x,4.75*inch,name_cn +"   " + name)
-
-	#line
-	c.setLineWidth(.3)
-	c.line(3*inch,4.65*inch,8*inch,4.65*inch)
-	c.line(3*inch,4.62*inch,8*inch,4.62*inch)
 
 	#scoring logic
 	c.setFont('STHeiti', 28)
@@ -96,21 +74,46 @@ def generate_certificate(entry):
 		level=level_2_cn
 
 	if score / total_score >= 0.9:
-		place = first_place
+		place = places[0]
 	elif 0.8 <= score / total_score < 0.9:
-		place = second_place
+		place = places[1]
 	elif 0.7 <= score / total_score < 0.8:
-		place = third_place
+		place = places[2]
 	else:
-		place = completion
+		place = places[3]
 	c.drawCentredString(half_x,4.25*inch,level+place)
+	 
+	#output
+	c.save()
+
+def draw_static_elements(c):
+	#background
+	img_path = 'images/background.jpg'
+	c.drawImage(image=img_path,x=0.25*inch,y=0.25*inch,width=10.5*inch,height=8.0*inch, mask='auto')
+	
+	#gold border
+	c.setStrokeColorRGB(0.999,0.799,0.066)
+	c.setFillColor(Color(100,100,100, alpha=0.25))
+	c.setLineWidth(3)
+	c.rect(0.0625*inch,0.0625*inch,10.875*inch,8.375*inch,fill=1)
+	c.setStrokeColorRGB(1.0,0.8,0.067)
+	c.setFillColor(Color(100,100,100, alpha=0.25))
+	c.setLineWidth(1)
+	c.rect(0.125*inch,0.125*inch,10.75*inch,8.25*inch,fill=1)
+	
+	#line
+	c.setLineWidth(.3)
+	c.setStrokeColor(black)
+	c.setFillColor(Color(0,0,0, alpha=1.0))
+	c.line(3*inch,4.65*inch,8*inch,4.65*inch)
+	c.line(3*inch,4.62*inch,8*inch,4.62*inch)
 
 	#footer 
 	c.setFont('Kaiti', 24)
 	c.drawCentredString(half_x,2.33*inch,committee_cn)
 	c.setFont('Helvetica', 20)
-	c.drawCentredString(half_x,2.00*inch,committee_en)
-	c.drawCentredString(half_x,1.67*inch,committee_en2)
+	c.drawCentredString(half_x,2.00*inch,committee_en[0])
+	c.drawCentredString(half_x,1.67*inch,committee_en[1])
 	c.drawCentredString(half_x,1.33*inch,date_of_completion)
 
 	#logo
@@ -125,9 +128,6 @@ def generate_certificate(entry):
 	c.drawCentredString(1.5*inch,2.05*inch,"JIMMY GAO")
 	img_path = 'images/stamp_transparent.png'
 	c.drawImage(image=img_path,x=1*inch,y=1*inch,width=inch,height=inch, mask='auto')
-	 
-	#output
-	c.save()
  
 #main
 if __name__ == '__main__':
